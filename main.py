19353ed0b2a89e887c0a7e13418e900e752c8fd8
Eternity1224 <elyseanato69@gmail.com>
2026-04-12 14:30:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def menu():
  print("3 - Ajouter un Album à un Artiste existant")
  print("4 - Statistiques et rapport")
  print("5 - Quitter l'application")
+
+def sous_menu_1():
+ print("a - Afficher tout les articles (nom, genre, pays, nombre d'album")
+ print("b - Rechercher un artiste par nom ou par genre")
+ print("c - Afficher le détail d'un artiste")
+
