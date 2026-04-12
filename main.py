2ae0740001e4e91405390407ba51e66c6f87698d
Eternity1224 <elyseanato69@gmail.com>
2026-04-12 14:26:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def hello():
- print("hello !")
-
-
-if __name__ = __main__:
- Hello()
+def menu():
+ print("1 - Consulter le catalogue")
+ print("2 - Ajouter un Article")
+ print("3 - Ajouter un Album à un Artiste existant")
+ print("4 - Statistiques et rapport")
+ print("5 - Quitter l'application")
